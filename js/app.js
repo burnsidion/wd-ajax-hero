fcf200f7b40878aa4cdddf57ab451e29a661fc29
ian.burnside89@gmail.com <YOUR EMAIL ADDRESSIan>
2018-02-25 01:28:26 -0700

    added one rule to input tag in html, added js

--- js/app.js
+++ js/app.js
@@ -17,7 +17,9 @@
         'data-tooltip': movie.title
       });
 
-      $title.tooltip({ delay: 50 }).text(movie.title);
+      $title.tooltip({
+        delay: 50
+      }).text(movie.title);
 
       const $poster = $('<img>').addClass('poster');
 
@@ -49,12 +51,47 @@
       $modal.append($modalContent);
 
       $col.append($card, $modal);
+      console.log($col);
+
 
       $('#listings').append($col);
 
       $('.modal-trigger').leanModal();
     }
   };
+  // ▼▼▼  set up event function for user input ▼▼▼
+  const textInput = (event) => {
+    //▼▼▼ Make sure the default refresh is deactivated ▼▼▼
+    event.preventDefault()
+    // ▼▼▼ Splice at first index and adding the length of the movies array ▼▼▼
+    movies.splice(0, movies.length)
+    // ▼▼▼ Create variable equal to the value of the input in the #search id. IE the text box. This will give the API something to search for later ▼▼▼
+    let title = $("#search").val()
+    //▼▼▼ Time to make a JSON request and create a function inside it which will recive data. This is where we append the Title variable we just created to the web API in the JSON request. We only want the web address up to the search input (?s=). ▼▼▼
+    $.getJSON(`https://omdb-api.now.sh/?s=${title}`, function(data) {
 
-  // ADD YOUR CODE HERE
+      //▼▼▼ Now to set up a forloop to loop through all the items we get back in the movies array, it will go for the length of the data we get back ▼▼▼
+      for (var i = 0; i < data.Search.length; i++) {
+        //▼▼▼ We'll now tell the forloop to push each data item it finds into our movies array
+        movies.push({
+          id: data.Search[i].imdbID,
+          poster: data.Search[i].Poster,
+          title: data.Search[i].Title,
+          year: data.Search[i].Year
+        })
+
+      }
+      console.log(data.Search);
+
+      renderMovies()
+
+
+
+
+    })
+  }
+// Waiting to fire all this off until the form hears a submit with textInput
+  $("form").submit(textInput);
+
+
 })();
